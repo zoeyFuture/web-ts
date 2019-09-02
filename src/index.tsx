78487e2833f3de42f8659d3567a4f1c5ed678776
Zhao Wei <zhaow@shinemo.com>
2019-09-02 20:19:04 +0800

    redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import { LocaleProvider } from 'antd'
 import zhCN from 'antd/lib/locale-provider/zh_CN'
+
+import reducer from '@src/redux'
+
+const store = createStore(reducer)
 
 import App from './App'
 
 ReactDOM.render(
   <LocaleProvider locale={zhCN}>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </LocaleProvider>,
   document.querySelector('#root') as HTMLElement,
 )
--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Table } from 'antd'
+import { Table, Button } from 'antd'
+import { connect } from 'react-redux'
 import PageLayout from '@components/PageLayout'
 import Breadcrumb from '@components/Breadcrumb'
 
@@ -7,7 +8,14 @@ import { getRoutes, getColumns } from './config'
 
 const { Header, Content } = PageLayout
 
-export default () => {
+const Home = (props: any) => {
+  const { counter } = props
+  console.log('props:', props)
+
+  const add = () => {
+    props.CounterIncrease(10)
+  }
+
   const routes = getRoutes()
   const columns = getColumns()
   return (
@@ -17,8 +25,30 @@ export default () => {
       </Header>
 
       <Content>
+        <span>{counter.counter}</span>
+        <Button onClick={add}>增加</Button>
         <Table columns={columns} dataSource={[]}/>
       </Content>
     </PageLayout>
   )
 }
+
+const mapStateToProps = (state: any): object => {
+  return {
+    counter: state.counter,
+  }
+}
+
+export const mapDispatchToProps = (dispatch: any) => {
+  return {
+    CounterIncrease: (payload: any) => {
+      dispatch({
+        payload,
+        type: "Increase"
+      })
+    }
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
